259df5b5a2735f2edc98a9d27cdbade9fe232ffe
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-03 15:58:56 +0100

    create marker works

--- src/MapView.tsx
+++ src/MapView.tsx
@@ -1,13 +1,27 @@
 import { useRef, useEffect } from 'react';
 import { initMap } from './utils/initMap';
+import { Map } from 'mapbox-gl';
+import { generateNewMarker } from './utils/generateNewMarker';
 
 export const MapView = () => {
 	const mapRef = useRef<HTMLDivElement>(null);
+	const mapInitRef = useRef<Map | null>(null);
 
 	useEffect(() => {
 		if (mapRef.current) {
-			initMap(mapRef.current, [13.3777,52.5163]);
+			mapInitRef.current = initMap(mapRef.current, [13.3777, 52.5163]);
 		}
+	}, []);
+
+	useEffect(() => {
+		mapInitRef.current &&
+			mapInitRef.current.on('dblclick', ({ lngLat }) =>
+				generateNewMarker({ map: mapInitRef.current!, ...lngLat })
+			);
+
+		return () => {
+			mapInitRef.current?.off('dblclick', generateNewMarker);
+		};
 	}, []);
 
 	return <div ref={mapRef} className="map" />;
